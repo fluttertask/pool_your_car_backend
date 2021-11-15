bea7dca32f2260bf28ffcfc0e8106cc81d6199b7
Jude Dickson <justclassic24@gmail.com>
2021-11-15 01:09:05 +0100

    fixed start rdie

--- routes/index.js
+++ routes/index.js
@@ -1131,17 +1131,8 @@ router.post("/api/ride/startride", (req, res) => {
                   id,
                   (err, user) => {
                     if (!err) {
-                      console.log(!user.notifications.includes({
-                        senderID: req.body.userId,
-                        type: 'startrequest',
-                        from: data.pickuplocation,
-                        to: data.droplocation,
-                        ride: req.body.rideId,
-                        message: `Accept to start your ride`
-                    }));
-                      // console.log(user);
                       if (user.notifications){
-                        if (user.notifications.includes({
+                        if (!user.notifications.includes({
                           senderID: req.body.userId,
                           type: 'startrequest',
                           from: data.pickuplocation,
